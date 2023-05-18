81253a2984e7637ff3f4e547f73b89876185a7a8
alexei199617 <44381914+alexei199617@users.noreply.github.com>
2023-05-18 17:19:02 +0300

    feat: add 3 thems

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,36 @@
 const box = document.querySelector('.box');
+let set = document.getElementById('setting');
 let sizeBox = 10; // 10, 15, 25
-let them = 'light';
+let them = '';
 let countClick = 0;
 
-if (countClick < 1) {
-    console.log(sizeBox);
+
+if(localStorage.getItem('them')) {
+    them = localStorage.getItem('them');
+    changeThem(them);
+} else {
+    them = 'themLight';
+    changeThem(them)
+}
+
+// Открытие окна настроек
+set.addEventListener('click', () => {
+    document.querySelector('.settingBoxShadow').classList.toggle('dNone');
+});
+// Смена темы
+document.querySelector('.settingBoxShadow').addEventListener('click', () => {
+    if (event.target.classList.contains('setVar') == true) {
+        document.querySelector('.settingBoxShadow').classList.toggle('dNone');
+        if (event.target.classList.contains('setVarLvl') == true) {
+            console.log('change ' + event.target.id)
+        } else {
+            changeThem(event.target.id);
+        }
+    } else if (event.target.classList.contains('settingBoxShadow') == true) {
+        document.querySelector('.settingBoxShadow').classList.toggle('dNone');
+    }
+});
+function changeThem (name) {
+    localStorage.setItem('them', name);
+    document.body.classList = name;
 }
