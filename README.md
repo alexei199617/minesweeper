5c12577873863c51168d1ed5b2cfe14ba0c4f4c7
Alexei <alexei199318@mail.ru>
2023-05-22 23:44:34 +0300

    feat: create branch minesweeper

--- README.md
+++
@@ -1,5 +0,0 @@
-# minesweeper
-
-npm add html-loader html-webpack-plugin --save-dev
-npx webpack build
-npx webpack build --progress
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const box = document.querySelector('.box');
-let sizeBox = 10; // 10, 15, 25
-let them = 'light';
-let countClick = 0;
-
-if (countClick < 1) {
-    console.log(sizeBox);
-}
